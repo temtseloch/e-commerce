f9611e39b17e0792c2fd9b3a455531b6d101097a
Temtsel-Och Ulziisaikhan <temkalways@gmail.com>
2021-01-13 09:55:13 -0500

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
