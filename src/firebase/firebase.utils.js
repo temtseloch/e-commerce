1b635ba7950fab7ee404f280a0fccce1a6ae87f3
Temtsel-Och Ulziisaikhan <temkalways@gmail.com>
2021-01-18 18:08:27 -0500

    fixed bugs

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,16 +11,18 @@ const config = {
   appId: "1:324161750378:web:bf98ac99029a804bc15113",
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
-
     try {
       await userRef.set({
         displayName,
@@ -29,14 +31,12 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData,
       });
     } catch (error) {
-      console.log(error.message);
+      console.log("error creating user", error.message);
     }
   }
 
   return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
